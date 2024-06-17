8d560c066bf17df219a6e886a07b2946edd77a7d
nataliaidk <127215791+nataliaidk@users.noreply.github.com>
2024-06-18 00:18:41 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, url_for, session
+from flask import Flask, render_template, redirect, request, url_for, session, flash
 import spotipy
 import spotipy.oauth2 as oauth2
 import os
@@ -9,10 +9,10 @@ app.secret_key = '8c12a131e8964aa8874bc0f5fe4560e8'  # Replace with a secret key
 SPOTIPY_CLIENT_ID = "8dd947abb4a341f3a58073753636b4bf"
 SPOTIPY_CLIENT_SECRET = "8c12a131e8964aa8874bc0f5fe4560e8"
 SPOTIPY_REDIRECT_URI = "http://127.0.0.1:8888/callback"
-scope = 'user-top-read'
+scope = 'user-top-read user-library-modify playlist-modify-public playlist-modify-private user-read-private user-read-email'
 
 
-sp_oauth = oauth2.SpotifyOAuth(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope='user-top-read', show_dialog=True)
+sp_oauth = oauth2.SpotifyOAuth(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope='user-top-read user-library-modify playlist-modify-public playlist-modify-private user-read-private user-read-email', show_dialog=True)
 
 def get_token():
     token_info = session.get('token_info', None)
@@ -30,6 +30,10 @@ def login():
     login_url = sp_oauth.get_authorize_url()
     return render_template('login.html', login_url=login_url)
 
+
+
+    
+
 @app.route('/callback')
 def callback():
     # Get the authorization code from the callback URL
@@ -41,6 +45,33 @@ def callback():
         return redirect(url_for('home'))
     else:
         return redirect(url_for('login'))
+    
+
+@app.route('/add_to_playlist', methods=['POST'])
+def add_to_playlist():
+    token = get_token()
+    if not token:
+        return redirect(url_for('login'))
+
+    sp = spotipy.Spotify(auth=token)
+    tracks = request.form.getlist('tracks')
+
+    user_id = sp.current_user()['id']
+    playlist_name = 'My New Playlist'
+    playlist_description = 'This is a new playlist created using Spotipy'
+    playlist = sp.user_playlist_create(user=user_id, name=playlist_name, public=True, description=playlist_description)
+    
+    # Przykład: dodanie utworów do playlisty
+    playlist_id = playlist['id']  # Zastąp tym ID swojej playlisty na Spotify
+     # Przetwórz listę tupli na listę URI
+    track_uris = [track[2] for track in tracks]
+    for track in track_uris:
+        print(track)
+    sp.playlist_add_items(playlist_id, track_uris)
+
+    flash('Tracks added to playlist successfully!', 'success')
+    return redirect(url_for('home'))
+    
     
 def get_top_songs(sp, time_range):
     results = sp.current_user_top_tracks(time_range=time_range, limit=30)
@@ -54,8 +85,17 @@ def get_recommended_tracks(sp, time_range):
     
     # Get recommended tracks based on top artists
     recommendations = sp.recommendations(seed_artists=artist_ids, limit=20)
-    recommended_tracks = [(track['name'], track['artists'][0]['name']) for track in recommendations['tracks']]
+    
+    recommended_tracks = []
+    for track in recommendations['tracks']:
+        track_name = track['name']
+        artist_name = track['artists'][0]['name']
+        track_uri = track['uri']
+        print(track_uri) 
+        recommended_tracks.append((track_name, artist_name, track_uri))
+    
     return recommended_tracks
+
 def get_prof_pic(sp, range):
     
     results = sp.current_user_top_artists(time_range=range, limit=20)
